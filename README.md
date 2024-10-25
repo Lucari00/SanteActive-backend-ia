060f295fc85d803dcba883447de7d6ea2bea9d49
Luca <luca.palaysi@gmail.com>
2024-10-26 00:50:50 +0200

    updated modelfile.py

--- README.md
+++ README.md
@@ -22,3 +22,11 @@ Launch the server with the following command. It will reload the server if you c
 You can use the API with the following command.
 
 ```curl -X POST "http://127.0.0.1:8000/api/generate" \ -H "Content-Type: application/json" \ -d "{\"messages\": {\"1\": \"Quelle est la météo aujourd'hui ?\", \"2\": \"Quelle heure est-il ?\"}}"```
+
+## Explaination
+
+The API is a FastAPI server.
+The model is based on llama3.1. If you don't have it, it will be downloaded automatically.
+
+The model is then personalized with the modelfile automatically. If the modelfile is modified, you need to delete the personalized model and create it again.
+You can delete the personalized model with python delete_perso_model.py.
--- app/models/modelfile.py
+++ app/models/modelfile.py
@@ -1,22 +1,24 @@
+from app.config import BASE_MODEL_NAME
+
 model_file = """
-FROM llama3.1
+FROM {base_model_name}
 PARAMETER temperature 1
 PARAMETER num_ctx 4096
 
 SYSTEM \"\"\"You are an assistant that receives questions in french in a json format but you don't answer them. You receive this json :
-{
-    "{question_id}": "question",
-}
-with {question_id} with the question_id being the id of the question. There can be multiple questions in the same json.
+{{
+    "$$question_id": "question",
+}}
+with $$question_id with the question_id being the id of the question. There can be multiple questions in the same json.
 
 The question are in french and you need to rephrase them in french also.
 Rephrase the questions simply. Questions should not be complicated or poorly worded. 
 If you find that they are already correct, don't change them. The goal is for you to modify them a little to avoid repetition.
 Your answer should always be the same. You have the format just below. It needs to be in a valid json format.
-{
-    "{question_id}": "phrase_rephrased",
-}
+{{
+    "$$question_id": "phrase_rephrased",
+}}
 Don't talk about the questions. Don't talk about the phrases. Just rephrase them in the json format given.
 Rephrase all of them.
 \"\"\"
-"""
+""".format(base_model_name=BASE_MODEL_NAME)
